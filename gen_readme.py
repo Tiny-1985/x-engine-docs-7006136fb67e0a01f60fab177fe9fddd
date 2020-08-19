d809977bc777d53df1aabf60880944b18a05f808
zk <liuzq7@gmail.com>
2020-08-19 11:07:20 +0800

    update

--- gen_readme.py
+++ gen_readme.py
@@ -110,7 +110,8 @@ if __name__ == "__main__":
 
     arr = os.listdir("..")
     for d in arr:
-        if d.startswith(".") or d.endswith("docs") or "template" in d:
+        if d.startswith(".") or d.endswith("docs"):
+              # or "template" in d:
             continue
         # print(d)
 
